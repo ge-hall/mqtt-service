20544eb8ec38e04d63999f35f085bcd4d872d7b3
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-09-19 22:32:53 +1000

    update formatting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import 'dotEnv/config';
-var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://test.mosquitto.org');
+import "dotEnv/config";
+var mqtt = require("mqtt");
+var client = mqtt.connect("mqtt://test.mosquitto.org");
 
-client.on('connect', function () {
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    }
-  })
-})
+client.on("connect", function() {
+    client.subscribe("presence", function(err) {
+        if (!err) {
+            client.publish("presence", "Hello mqtt");
+        }
+    });
+});
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+client.on("message", function(topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+    client.end();
+});
