b655093fdc163a5a92c663afb2896facfa41c9ef
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-09-28 23:32:36 +1000

    added yalm log added several event types removed client.end now continues to listen for topics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import "dotEnv/config";
-var mqtt = require("mqtt");
-var client = mqtt.connect("mqtt://test.mosquitto.org");
-
-client.on("connect", function() {
-    client.subscribe("presence", function(err) {
+import 'dotenv/config';
+const log = require('yalm');
+var Mqtt = require("mqtt");
+//var client = mqtt.connect("mqtt://test.mosquitto.org");
+//MQTT_SERVER
+var mqtt = Mqtt.connect(process.env.MQTT_SERVER);
+mqtt.on("connect", function() {
+    mqtt.subscribe("presence", function(err) {
         if (!err) {
-            client.publish("presence", "Hello mqtt");
+            mqtt.publish("presence", "Hello mqtt");
         }
     });
 });
 
-client.on("message", function(topic, message) {
+mqtt.on("message", function(topic, message) {
     // message is Buffer
     console.log(message.toString());
-    client.end();
+    //client.end();
+});
+
+mqtt.on('error', err => {
+    log.error('mqtt', err);
+});
+
+mqtt.on('close', () => {
+    log.warn('mqtt close');
+});
+
+mqtt.on('offline', () => {
+    log.warn('mqtt offline');
+});
+
+mqtt.on('reconnect', () => {
+    log.info('mqtt reconnect');
 });
